16b62751b41b727276521c4945bc3036a334f232
LesiaBelinska <lm.kudak@gmail.com>
2022-05-22 12:04:49 +0300

    delete persistStore

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,43 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
-import {
-persistStore,
-persistReducer,
-  FLUSH,
-  REHYDRATE,
-  PAUSE,
-  PERSIST,
-  PURGE,
-  REGISTER,
-} from 'redux-persist'
-//import logger from "redux-logger";
-import storage from "redux-persist/lib/storage";
 import contactsReducer from "./contacts-reducer";
-
-const contactsPersistConfig = {
-    key: 'contacts',
-    storage,
-    blacklist: ['filter'], //blacklist - зберігати окрім такого поля, whitelist - зберігати тільки такі поля
-};
-
-export const persistedReducer = persistReducer(
-    contactsPersistConfig,
-    contactsReducer,
-);
 
 export const store = configureStore({
     
-    reducer: persistedReducer,
-    middleware(getDefaultMiddleware) {
-        return getDefaultMiddleware({
-            serializableCheck: {
-                ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-            },
-        });
-    },
-    // прослойка, для виводу у консоль
-    // middleware: getDefaultMiddleware =>
-    //     [...getDefaultMiddleware(), logger],
+    reducer: contactsReducer,
 });
-
-export const persistor = persistStore(store);
 
