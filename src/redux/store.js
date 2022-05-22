6f37b1a17f66c126b552cace634606f3dc9bf2b4
LesiaBelinska <lm.kudak@gmail.com>
2022-05-23 02:17:17 +0300

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,23 @@
-import { configureStore } from "@reduxjs/toolkit";
-import contactsReducer from "./contacts-reducer";
+import { configureStore } from '@reduxjs/toolkit';
+import { contactsApi } from './contactsSlice.js';
+import { filterSlice } from './filterSlice.js';
 
 export const store = configureStore({
-    
-    reducer: contactsReducer,
+  reducer: {
+    [contactsApi.reducerPath]: contactsApi.reducer,
+    filter: filterSlice.reducer,
+  },
+  middleware: getDefaultMiddleware => [
+    ...getDefaultMiddleware(),
+    contactsApi.middleware,
+  ],
 });
+
+// import { configureStore } from "@reduxjs/toolkit";
+// import contactsReducer from "./contacts-reducer";
+
+// export const store = configureStore({
+    
+//     reducer: contactsReducer,
+// });
 
