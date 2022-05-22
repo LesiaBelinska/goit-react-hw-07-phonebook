049be0d925dd0d25ab132890ff50df9654cbc311
LesiaBelinska <lm.kudak@gmail.com>
2022-05-23 02:16:33 +0300

    Update contacts-reducer.js

--- src/redux/contacts-reducer.js
+++ src/redux/contacts-reducer.js
@@ -1,26 +1,26 @@
-import { createReducer, combineReducers } from "@reduxjs/toolkit";
-//import { combineReducers } from "redux";
+// import { createReducer, combineReducers } from "@reduxjs/toolkit";
+// //import { combineReducers } from "redux";
 
-import { addContact, deleteContact, changeFilter } from "./contacts-actions.js";
-
-
-const contactsReducer = createReducer([], {
-    [addContact]: (state, action) => {
-        return [...state, action.payload]
-    },
-    [deleteContact]: (state, action) => {
-        return state.filter(contact => contact.id !== action.payload);
-    },
-});
+// import { addContact, deleteContact, changeFilter } from "./contacts-actions.js";
 
 
-const filterReducer = createReducer('', {
-    [changeFilter]: (_, action) => {
-        return action.payload
-    },
-});
+// const contactsReducer = createReducer([], {
+//     [addContact]: (state, action) => {
+//         return [...state, action.payload]
+//     },
+//     [deleteContact]: (state, action) => {
+//         return state.filter(contact => contact.id !== action.payload);
+//     },
+// });
 
-export default combineReducers({
-    contacts: contactsReducer,
-    filter: filterReducer,
-});
+
+// const filterReducer = createReducer('', {
+//     [changeFilter]: (_, action) => {
+//         return action.payload
+//     },
+// });
+
+// export default combineReducers({
+//     contacts: contactsReducer,
+//     filter: filterReducer,
+// });
