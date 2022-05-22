2df97162afa30f99a7f9ee530f6e6cba67700210
LesiaBelinska <lm.kudak@gmail.com>
2022-05-23 02:18:05 +0300

    Update ContactListItem.jsx

--- src/components/ContactListItem/ContactListItem.jsx
+++ src/components/ContactListItem/ContactListItem.jsx
@@ -1,16 +1,21 @@
 import PropTypes from 'prop-types';
 
+import { useDeleteContactMutation } from 'redux/contactsSlice';
 import s from "./ContactListItem.module.css";
 
-const ContactListItem = ({ id, name, number, onDeleteContact }) => {
+const ContactListItem = ({ id, name, number }) => {
+
+    const [deleteContact, result] = useDeleteContactMutation();
     return (
         <li className={s.item}>{name}: {number}
             <button className={s.button} type="button"
-                onClick={() => onDeleteContact(id)}>
+                onClick={() => deleteContact(id)}
+                disabled={result.isLoading}
+            >
                 Delete
             </button></li>
     )
-};
+}
 
 export default ContactListItem;
 
@@ -18,5 +23,5 @@ ContactListItem.propTypes = {
     id: PropTypes.string.isRequired,
     name: PropTypes.string.isRequired,
     number: PropTypes.string.isRequired,
-    onDeleteContact: PropTypes.func.isRequired,
+    //onDeleteContact: PropTypes.func.isRequired,
 };
