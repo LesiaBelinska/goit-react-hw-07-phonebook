51c47b67b6bcbe767e6157b747ceee006a1cdb2b
LesiaBelinska <lm.kudak@gmail.com>
2022-05-24 12:16:31 +0300

    Update ContactList.jsx

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -18,56 +18,22 @@ const ContactList = () => {
         <>
             {contacts ? isFetching : <p className={s.loading}>Loading...</p>}
             <ul>
-            {contacts && filteredContacts.map(({ id, name, phone }) => {
-                return (
-                    <ContactListItem
-                        key={id}
-                        id={id}
-                        name={name}
-                        number={phone}
-                    />
-                )
-            })}
-        </ul>
+                {contacts && filteredContacts.map(({ id, name, phone }) => {
+                    return (
+                        <ContactListItem
+                            key={id}
+                            id={id}
+                            name={name}
+                            number={phone}
+                        />
+                    )
+                })}
+            </ul>
         </>
-    )
+    );
 }
 
 export default ContactList;
 
 
-
-//BEFORE
-
-// import { useSelector, useDispatch } from 'react-redux';
-
-// import ContactListItem from 'components/ContactListItem/ContactListItem';
-// import { deleteContact } from "../../redux/contacts-actions.js";
-
-// const ContactList = () => {
-
-//     const contacts = useSelector(state => state.contacts);
-//     const filter = useSelector(state => state.filter);
-//     const dispatch = useDispatch();
-//     const filteredContacts = contacts?.filter(contact =>
-//         contact.name.toLocaleLowerCase().includes(filter.toLocaleLowerCase()));
-
-//     return (
-//         <ul>
-//             {filteredContacts.map(({ id, name, number }) => {
-//                 return (
-//                     <ContactListItem
-//                         key={id}
-//                         id={id}
-//                         name={name}
-//                         number={number}
-//                         onDeleteContact={(contactId)=>dispatch(deleteContact(contactId))}
-//                     />
-//                 )
-//             })}
-//         </ul>
-//     )
-// };
-
-// export default ContactList;
 
